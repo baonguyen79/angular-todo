8cb755c6fef7782d85e3a496e5821609acf06321
nguyen,bao <bao.nguyen@ingramcontent.com>
2017-05-16 19:32:30 -0500

    delete

--- javascripts/controllers/ItemListCtrl.js
+++ javascripts/controllers/ItemListCtrl.js
@@ -1,11 +1,21 @@
 app.controller("ItemListCtrl", function($scope, ItemFactory) {
    $scope.items = [];
 
-    ItemFactory.getItemList().then((itemz) => {
-      $scope.items = itemz;
-      console.log("item", itemz);
+    let getItems = () => {
+     	ItemFactory.getItemList().then((itemz) => {
+        $scope.items = itemz;
     }).catch((error) => {
       console.log("get error" , error)
     });
+};
+
+	getItems();
  
+ 	$scope.deleteItem = (id) => {
+ 		ItemFactory.deletz(id).then(() => {
+ 		getItems();
+ 		}).catch((error) => {
+ 			console.log("deleteItem error", error);
+ 		});
+ 	};
 });
--- javascripts/factories/ItemFactory.js
+++ javascripts/factories/ItemFactory.js
@@ -1,4 +1,3 @@
-// app.factory("ItemFactory", function($q, $http, FIREBASE_CONFIG){
 app.factory("ItemFactory", function($http, $q, FIREBASE_CONFIG){  
   let getItemList = () => {
     let itemz = [];
@@ -29,7 +28,22 @@ app.factory("ItemFactory", function($http, $q, FIREBASE_CONFIG){
     });
   };
 
-  return {getItemList:getItemList, postNewItem:postNewItem};
+  let deletz = (itemId) => {
+    return $q((resolve, reject) => {
+      $http.delete(`${FIREBASE_CONFIG.databaseURL}/items/${itemId}.json`)
+      .then((resultz) => {
+          resolve(resultz);
+        }).catch((error) => {
+          reject(error);
+        });
+    })
+  };
+
+
+
+
+
+  return {getItemList:getItemList, postNewItem:postNewItem, deletz:deletz};
 
 
 });
